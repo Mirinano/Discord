7c680ece0a48bf05e77e63d166de1218a0f565af
Mirinano <43010381+Mirinano@users.noreply.github.com>
2019-12-04 12:19:37 +0900

    Create cmd_trigger.py

---
+++ cmd_trigger.py
@@ -0,0 +1,37 @@
+#!python3.6
+cmd_help="/help", "op2" #ok
+#op1
+translation="!t ", "op1" #ok
+add_role="+", "op1" #ok
+add_color="+color", "op1" #ok
+remove_role="-", "op1" #ok
+remove_color="-color", "op1" #ok
+#op2
+ban="/ban", "op2" #ok
+unban="/unban", "op2" #ok
+kick="/kick", "op2" #ok
+stop="/stop", "op2" #ok
+send_dm="/send-dm", "op2" #ok
+edit_dm="/edit-dm", "op2" #ok
+del_dm="/del-dm", "op2" #ok
+get_dm="/get-dm", "op2"#ok
+statistics="/statistics", "op2" #ok simple,normal
+user="/user", "op2" #ok
+#op3
+spam="/spam", "op3" #ok
+alert="/alert", "op3" #ok
+send_msg="/send-msg", "op3" #ok
+edit_msg="/edit-msg", "op3" #ok
+get_msg_log="/get-log", "op3" #ok
+ls="/ls", "op3" #ok
+system_message="/system-message", "op3" #ok
+#op4
+send_zip="$$send-log$$", "op4" #ok
+send_today_zip="$$send-log-today$$", "op4" #ok
+stop_server="$$stop-server$$", "op4"
+restart_server="$$restart-server$$", "op4"
+mass_spam="$$global-spam$$", "op4" #ok
+
+#memo
+## receive dm method ok
+## reaction system ok

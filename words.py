acf31b8a9a21a1878514f9fec50c8ac2acf38120
Mirinano <43010381+Mirinano@users.noreply.github.com>
2019-12-04 12:21:38 +0900

    Create words.py

---
+++ words.py
@@ -0,0 +1,18 @@
+send_messages = "メッセージを送信 (Send Messages)"
+read_message_history = "メッセージ履歴を閲覧 (Read MEssage History)"
+
+unspecified = "未指定"
+
+no_action = "対処無し"
+
+report = "報告"
+emergency = "緊急"
+warning = "警告"
+attention = "注意"
+
+#spam/alert report
+dealing_with_spam = "スパムを対処"
+spam_occurrence = "スパム発生"
+attention_message = "要注意メッセージ"
+ban_succsess = "BANを実施"
+ban_fail = "BAN失敗"

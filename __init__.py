5d7e32b22566e491c3faf338c29f520417d2a85a
Mirinano <43010381+Mirinano@users.noreply.github.com>
2019-12-04 12:17:40 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+__title__ = 'Discord Bot Code'
+__author__ = 'Mirinano'
+__copyright__ = 'Copyright 2019 Mirinano'
+__version__ = '1.0.0' #2019/12/4
+
+from collections import namedtuple
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel='final', serial=0)
+
+class Developer():
+    name = "Mirinano"
+    id = ""
